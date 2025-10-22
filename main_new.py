2ad83cb6c190152a6fbe1290749a6a733b000797
Korniflex <alexander.leypold@data-craft-students.de>
2025-10-22 12:21:34 +0200

    Vorbereitung online database

--- main_new.py
+++ main_new.py
@@ -13,11 +13,11 @@ import hashlib
 
 # PostgreSQL Verbindung:
 conn = psycopg2.connect(
-    dbname="JobScout",
-    user="postgres",
-    password="postgres",
-    host="localhost",
-    port="5433"
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT")
 )
 cursor = conn.cursor()
 
